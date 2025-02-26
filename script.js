6a57d115b3dff9b6676921620e2cbfff0a91cc41
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-02-26 21:02:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 // complete this js code
-function Person(name, age) {}
+class Person{
+	constructor(name , age){
+		this._name = name;
+		this._age = age;
+	}
 
-function Employee(name, age, jobTitle) {}
+	greet(){
+		console.log(`Hello, my name is ${this._name} and I am ${this._age} years old.`);
+	}
+}
+
+class Employee extends Person{
+	constructor(name, age, jobTitle){
+		super(name, age);
+		this._jobTitle = jobTitle;
+	}
+
+	jobGreet(){
+		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
+	}
+}
+
+
+const person = new Person("Alice", 25);
+person.greet();
+
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
